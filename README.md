3f6aab3aaac2b225ff3cd1fe304848711812af21
Gabi Procell <gabi@echobind.com>
2019-05-02 11:01:16 -0600

    Basic CRUD endpoint setup

---
+++ README.md
@@ -0,0 +1,4 @@
+## Resources used for development:</br>
+[Build A RESTful Api With Node.js And Express.Js Part Two](https://medium.com/@purposenigeria/build-a-restful-api-with-node-js-and-express-js-part-two-3d7a82b8e00)
+
+[Build A RESTful Api With Node.js And Express.Js Part One](https://medium.com/@purposenigeria/build-a-restful-api-with-node-js-and-express-js-d7e59c7a3dfb)
--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 import express from 'express';
+// Body parser parses incoming request bodies in a middleware before handlers. 
+// It parses the response data coming from the client in an object called req.body
+import bodyParser from 'body-parser';
 import db from './db/db';
 
 // Set up the express app
 const app = express();
 
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// <---- GET REQUESTS ---->
 // get all todos
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
@@ -11,6 +19,124 @@ app.get('/api/v1/todos', (req, res) => {
     message: 'todos retrieved successfully',
     todos: db
   })
+});
+
+// get a todo by ID
+app.get('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((todo) => {
+    if(todo.id === id) {
+      return res.status(200).send({
+        success: 'true',
+        message: 'todo retrieved successfully',
+        todo,
+      });
+    }
+  });
+
+  return res.status(404).send({
+    success: 'false',
+    message: 'todo does not exist',
+  });
+
+})
+
+// <---- POST REQUESTS ---->
+
+// create a todo
+app.post('/api/v1/todos', (req, res) => {
+  if(!req.body.title) {
+    return res.statusMessage(400).send({
+      success: 'false',
+      message: 'title is required',
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+  const todo = {
+    id: db.length + 1,
+    title: req.body.title,
+    description: req.body.description
+  }
+  db.push(todo);
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo added successfully',
+    todo
+  })
+});
+
+// <---- UPDATE REQUESTS ---->
+app.put('api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+  db.map((todo, index) => {
+    if(todo.id === id) {
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+
+  if(!todoFound) {
+    return res.status(404).send({
+      success: 'false',
+      message: 'todo not found',
+    });
+  }
+
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required',
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required',
+    });
+  }
+
+  const updatedTodo = {
+    id: todoFound.id,
+    title: req.body.title || todoFound.title,
+    description: req.body.description || todoFound.description,
+  };
+  
+  db.splice(itemIndex, 1, updatedTodo);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo added successfully',
+    updatedTodo,
+  });
+});
+
+// <---- DELETE REQUESTS ---->
+
+// delete a todo
+app.delete('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      db.splice(index, 1);
+      return res.status(200).send({
+        success: 'true',
+        message: 'Todo deleted successfully',
+      });
+    }
+  });
+
+  return res.status(404).send({
+    success: 'false',
+    message: 'todo not found',
+  });
+
 });
 
 const PORT = 5000;
